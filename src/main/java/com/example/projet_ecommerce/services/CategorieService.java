0e6c49429961f670717827e95fd1aaa5e8df19c1
asmaakbouchi <asmaakbouchi@gmail.com>
2024-01-16 19:45:02 +0100

    first commit

---
+++ src/main/java/com/example/projet_ecommerce/services/CategorieService.java
@@ -0,0 +1,16 @@
+package com.example.projet_ecommerce.services;
+
+import com.example.projet_ecommerce.entities.Categorie;
+
+import java.util.List;
+
+public interface CategorieService {
+
+    Categorie saveCategorie(Categorie categorie);
+    Categorie getCategorieByUuidCategorie(String uuid);
+
+    void validateCategorie(Categorie categorie,  boolean isUpdate);
+    void deleteCategorieByUuidCategorie(String uuid);
+   Categorie updateCategorie(String uuid, Categorie updatedCategorie);
+    List<Categorie> getAllCategories();
+}
